d752bf08e2c35512b2bd46d9747bb4e986065a78
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2025-03-16 23:52:39 +0300

    IUserRepository Added

---
+++ ChromaVision.Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using ChromaVision.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChromaVision.Domain.Repositories
+{
+    public interface IUserRepository : IRepository<User>
+    {
+        Task<User> GetByUsernameAsync(string username);
+        Task<User> GetByEmailAsync(string email);
+        Task<bool> UsernameExistsAsync(string username);
+        Task<bool> EmailExistsAsync(string email);
+    }
+}

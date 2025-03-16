acbb9c3fd204baef9fc75e99f669848a8606a9a9
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2025-03-16 23:45:12 +0300

    User Added

---
+++ ChromaVision.Domain/Entities/User.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChromaVision.Domain.Entities
+{
+    public class User
+    {
+        public Guid Id { get; private set; }
+        public string Username { get; private set; }
+        public string Email { get; private set; }
+        public string PasswordHash { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public DateTime? LastLoginAt { get; private set; }
+
+        protected User() { } // For EF Core
+
+        public User(string username, string email, string passwordHash)
+        {
+            Id = Guid.NewGuid();
+            Username = username ?? throw new ArgumentNullException(nameof(username));
+            Email = email ?? throw new ArgumentNullException(nameof(email));
+            PasswordHash = passwordHash ?? throw new ArgumentNullException(nameof(passwordHash));
+            CreatedAt = DateTime.UtcNow;
+        }
+
+        public void UpdateUsername(string username)
+        {
+            if (string.IsNullOrWhiteSpace(username))
+                throw new ArgumentException("Username cannot be empty", nameof(username));
+
+            Username = username;
+        }
+
+        public void UpdateEmail(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+                throw new ArgumentException("Email cannot be empty", nameof(email));
+
+            Email = email;
+        }
+
+        public void UpdatePasswordHash(string passwordHash)
+        {
+            if (string.IsNullOrWhiteSpace(passwordHash))
+                throw new ArgumentException("Password hash cannot be empty", nameof(passwordHash));
+
+            PasswordHash = passwordHash;
+        }
+
+        public void RecordLogin()
+        {
+            LastLoginAt = DateTime.UtcNow;
+        }
+    }
+}

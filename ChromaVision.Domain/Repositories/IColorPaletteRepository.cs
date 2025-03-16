fc55d845564aa8ea5e08ef0445c121d939ef4f5b
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2025-03-16 23:51:16 +0300

    IColorPaletteRepository Added

---
+++ ChromaVision.Domain/Repositories/IColorPaletteRepository.cs
@@ -0,0 +1,15 @@
+﻿using ChromaVision.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChromaVision.Domain.Repositories
+{
+    public interface IColorPaletteRepository : IRepository<ColorPalette>
+    {
+        Task<IReadOnlyList<ColorPalette>> GetByUserIdAsync(Guid userId);
+        Task<IReadOnlyList<ColorPalette>> SearchByNameAsync(string searchTerm);
+    }
+}

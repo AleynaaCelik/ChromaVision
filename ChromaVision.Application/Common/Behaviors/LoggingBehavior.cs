b57408dbd97a1294e17cc85a490328cfbc1ad241
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2025-03-18 23:29:06 +0300

    Behaviors Added

---
+++ ChromaVision.Application/Common/Behaviors/LoggingBehavior.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChromaVision.Application.Common.Behaviors
+{
+    public class LoggingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+         where TRequest : notnull
+    {
+        private readonly IAppLogger<LoggingBehavior<TRequest, TResponse>> _logger;
+
+        public LoggingBehavior(IAppLogger<LoggingBehavior<TRequest, TResponse>> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            var requestName = typeof(TRequest).Name;
+            _logger.LogInformation($"Handling {requestName}");
+
+            var stopwatch = Stopwatch.StartNew();
+            var response = await next();
+            stopwatch.Stop();
+
+            _logger.LogInformation($"Handled {requestName} in {stopwatch.ElapsedMilliseconds}ms");
+
+            return response;
+        }
+    }
+}
--- ChromaVision.Application/DependencyInjection.cs
+++ ChromaVision.Application/DependencyInjection.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using ChromaVision.Application.Common.Behaviors;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;

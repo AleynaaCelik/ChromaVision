2bedb1075848f92f2276784d737790e6f9b6464f
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2025-03-18 23:32:39 +0300

    IJwtService Created

---
+++ ChromaVision.Application/Common/Interfaces/IJwtService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChromaVision.Application.Common.Interfaces
+{
+    public interface IJwtService
+    {
+        Task<string> GenerateTokenAsync(Guid userId, string username);
+        Task<(bool isValid, Guid userId)> ValidateTokenAsync(string token);
+    }
+}

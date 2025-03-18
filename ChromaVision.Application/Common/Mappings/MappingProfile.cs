00ecdc59ffe63a1fc2850c999e26c2bc2f7247e6
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2025-03-18 23:40:41 +0300

    Chroma Vision.Application - Application Layer (Common Components) Added

---
+++ ChromaVision.Application/Common/Mappings/MappingProfile.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using ChromaVision.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChromaVision.Application.Common.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // Color Palette mappings
+            CreateMap<ColorPalette, ColorPaletteDto>()
+                .ForMember(dest => dest.Colors, opt => opt.MapFrom(src => src.Colors.Select(c => c.HexCode).ToList()));
+
+            CreateMap<CreatePaletteCommand, ColorPalette>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+                .ForMember(dest => dest.UpdatedAt, opt => opt.Ignore())
+                .ForMember(dest => dest.Colors, opt => opt.Ignore());
+
+            // User mappings
+            CreateMap<User, UserDto>();
+
+            CreateMap<RegisterUserCommand, User>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+                .ForMember(dest => dest.LastLoginAt, opt => opt.Ignore())
+                .ForMember(dest => dest.PasswordHash, opt => opt.Ignore());
+        }
+    }
+}
---
+++ ChromaVision.Application/Common/Mappings/UserDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChromaVision.Application.Common.Mappings
+{
+    public class UserDto
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? LastLoginAt { get; set; }
+    }
+}

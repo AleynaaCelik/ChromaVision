e924613ad4538a129d135510b4dc9f72649c4eea
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2025-03-18 23:31:17 +0300

    IImageProcessingService Created

---
+++ ChromaVision.Application/Common/Interfaces/IImageProcessingService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChromaVision.Application.Common.Interfaces
+{
+    public interface IImageProcessingService
+    {
+        Task<List<string>> ExtractColorsFromImageAsync(Stream imageStream, int colorCount = 5, CancellationToken cancellationToken = default);
+    }
+}

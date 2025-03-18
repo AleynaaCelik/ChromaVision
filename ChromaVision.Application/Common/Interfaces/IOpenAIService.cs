dfd19a9af4fe8e69d98d275ba8c577b93656f3b5
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2025-03-18 23:30:21 +0300

    IOpenAIService Created

---
+++ ChromaVision.Application/Common/Interfaces/IOpenAIService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChromaVision.Application.Common.Interfaces
+{
+    public interface IOpenAIService
+    {
+        Task<List<string>> GenerateColorsFromDescriptionAsync(string description, int colorCount = 5, CancellationToken cancellationToken = default);
+    }
+}

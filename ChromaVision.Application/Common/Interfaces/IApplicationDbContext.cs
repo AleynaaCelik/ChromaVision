e1b987bc3b8f7dbc756591a76f9f2d5de26a2dc7
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2025-03-20 23:37:22 +0300

    ChromaVision.Application - Interface Definitions

---
+++ ChromaVision.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using ChromaVision.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChromaVision.Application.Common.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<ColorPalette> ColorPalettes { get; }
+        DbSet<ColorItem> ColorItems { get; }
+        DbSet<User> Users { get; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}

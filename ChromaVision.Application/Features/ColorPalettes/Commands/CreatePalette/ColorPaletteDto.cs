8f52606be0a478cf8ab7907746d97a47dfbd987e
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2025-03-22 22:26:44 +0300

    ChromaVision.Application - CreatePaletteCommand

---
+++ ChromaVision.Application/Features/ColorPalettes/Commands/CreatePalette/ColorPaletteDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChromaVision.Application.Features.ColorPalettes.Commands.CreatePalette
+{
+    public class ColorPaletteDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public List<string> Colors { get; set; } = new List<string>();
+        public Guid? UserId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
